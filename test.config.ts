48ac925560b5a1aa1b1da94d7caaa4cd97fe553b
Huy Quoc Pham <pqhuy0211@gmail.com>
2026-08-08 14:45:01 +0700

    Init Framework
    Setup Project Structure
    Install dependencies
    Update config files

---
+++ test.config.ts
@@ -0,0 +1,10 @@
+export class TestConfig {
+  appUrl = "http://localhost";
+  //valid login credentials- create your own login account
+  email = "huy@gmail.com";
+  password = "123456";
+  //product details
+  productName = "MacBook";
+  productQuantity = "2";
+  totalPrice = "$1,204.00";
+}
